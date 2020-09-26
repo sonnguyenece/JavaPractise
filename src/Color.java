d3d1fabf43b732da2d54612ca837549c59210886
nmson <nmson@ecpay.vn>
2020-09-26 10:24:15 +0700

    enum with abstract method

---
+++ src/Color.java
@@ -0,0 +1,35 @@
+public enum Color {
+
+    RED("red") {
+        @Override
+        public String getHexCode() {
+            return "#ff0000";
+        }
+    },
+    GREEN("green") {
+        @Override
+        public String getHexCode() {
+            return "#00ff00";
+        }
+    },
+    BLUE("blue") {
+        @Override
+        public String getHexCode() {
+            return "#0000ff";
+        }
+    };
+
+    private String name;
+
+    Color(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+    public abstract String getHexCode();
+
+}
