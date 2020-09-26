6b47a62d4f85fb42c107fcc5431289d6362d3a4c
nmson <nmson@ecpay.vn>
2020-09-26 10:45:47 +0700

    @Deprecated

---
+++ src/DeprecatedClass.java
@@ -0,0 +1,9 @@
+public class DeprecatedClass {
+    @Deprecated
+     public String todoOld(){
+        return "doing shit";
+    }
+    public String todo(){
+         return "doing poop";
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        DeprecatedClass deprecatedClass =new DeprecatedClass();
+        System.out.println(deprecatedClass.todoOld());
+        deprecatedClass.todo();
     }
 }
