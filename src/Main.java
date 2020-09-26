19ddfd4e14713ffb70b1725091c66c1319abf418
nmson <nmson@ecpay.vn>
2020-09-26 09:22:31 +0700

    init

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(getJob(WeekDayConstants.MONDAY));
+//        System.out.println(getJob(WeekDayConstants.MONDAY));
+
+        System.out.println(Arrays.toString(WeekDayConstants.values()));
+        for (WeekDayConstants day : WeekDayConstants.values()
+        ) {
+            System.out.println(day);
+        }
+
+//        List<WeekDayConstants> day = Arrays.asList(WeekDayConstants.values());
+//        day.forEach(System.out::println);
+
     }
 
     public static String getJob(WeekDayConstants weekDay) {
