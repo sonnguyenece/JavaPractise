503935cb6542b4477a70b6adc09e06cd0eaf4c4f
nmson <nmson@ecpay.vn>
2020-09-26 10:41:23 +0700

    init

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,4 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(getJob(WeekDayConstants.MONDAY));
-    }
-
-    public static String getJob(WeekDayConstants weekDay) {
-        if (weekDay == WeekDayConstants.SATURDAY || weekDay == WeekDayConstants.SUNDAY) {
-            return "Day off";
-        }
-
-        return "Coding day";
     }
 }
